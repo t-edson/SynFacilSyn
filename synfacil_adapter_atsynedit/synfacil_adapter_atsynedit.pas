4861f7824ff4ad66c2214484a715302f95f22aab
Tito Hinostroza <dev_null_1@hotmail.com>
2015-09-12 21:55:48 -0500

    Testing ranges

--- synfacil_adapter_atsynedit/synfacil_adapter_atsynedit.pas
+++ synfacil_adapter_atsynedit/synfacil_adapter_atsynedit.pas
@@ -24,6 +24,8 @@ type
     hlt    : TSynFacilSyn;
     states : TSynFacilLineStates;
     fRange : TPtrTokEspec;
+    firstModified: Integer;
+    procedure ScanLines(ed: TATSynEdit; const scanFrom, scanTo: integer);
   public
     procedure SetSyntax(const AFilename: string);
     procedure StringsLog(Sender: TObject; ALine, ALen: integer);
@@ -76,9 +78,20 @@ var
   ed: TATSynEdit;
   Str: atString;
   atr: TSynHighlighterAttributes;
+  ist: Integer;
 begin
+  DebugLn('-OnEditorCalcHilite: ALineIndex=' + IntToStr(ALineIndex));
   ed:= Sender as TATSynEdit;
   Str:= Copy(ed.Strings.Lines[ALineIndex], ACharIndex, ALineLen);
+  if ALineIndex mod LINES_PER_STATE = 0 then begin
+    ist := ALineIndex div LINES_PER_STATE - 1;
+    DebugLn('OnCalcHilite: Getting state for line:'+ IntToStr(ALineIndex+1) +
+                      ' from states[' + IntToStr(ist)+']');
+    if ist = -1 then
+      hlt.Range := nil
+    else
+      hlt.Range := states[ist];
+  end;
   hlt.SetLine(Str, ALineIndex);
   npart:= 0;
   noffset:= 0;
@@ -101,24 +114,50 @@ end;
 
 procedure TSynFacilAdapter.StringsLog(Sender: TObject; ALine, ALen: integer);
 begin
-  DebugLn('OnLog: ALine=' + IntToStr(ALine) + ' ALen=' + IntToSTr(Alen));
+  if (Aline>=0) and (Aline<firstModified) then firstModified := ALine;
+//  DebugLn('OnLog: ALine=' + IntToStr(ALine) + ' ALen=' + IntToSTr(Alen));
 end;
-
+procedure TSynFacilAdapter.ScanLines(ed: TATSynEdit; const scanFrom, scanTo: integer);
+{Scans an interval of lines to update the state of the range in States[].
+ States[] save the state of the highlighter at the end of the exploration. It's
+ saved only for every LINES_PER_STATE lines, for to save memory and avoid to change
+ frecuently the size of the list states[]. }
+var
+  i: Integer;
+  ist: Integer;
+begin
+  DebugLn('  ScanLines: scanning from:' + IntToStr(scanFrom+1)+ ' to ' + IntToStr(scanTo+1));
+  //scan necessary lines
+  for i:= scanFrom to scanTo do
+  begin
+    hlt.SetLine(ed.Strings.lines[i], i);
+    while true do begin
+      if hlt.GetEol then begin
+        if (i+1) mod LINES_PER_STATE = 0 then begin
+          ist := (i+1) div LINES_PER_STATE;
+          DebugLn('  ScanLines: saving state for line:' + IntToStr(i+1) +
+                  ' in states[' + IntToStr(ist) + ']');
+          states[ist] := hlt.Range;
+        end;
+        break;
+      end;
+      hlt.Next;
+    end;
+  end;
+end;
 procedure TSynFacilAdapter.OnEditorChange(Sender: TObject);
 var
   ed: TATSynEdit;
   Needed: Integer;
-  firstModified: Integer;
   scanFrom: Integer;
   scanTo: Integer;
-  i: Integer;
   stateFrom: Integer;
 begin
   ed:= Sender as TATSynEdit;
   //Calculate necessary lines
-  Needed := ed.Strings.Count div LINES_PER_STATE;
-  DebugLn('Change: ed with lines:' + IntToStr(ed.Strings.Count) +
-                   'Needed: '+ IntToStr(Needed));
+  Needed := ed.Strings.Count div LINES_PER_STATE + 1;
+  DebugLn('EditorChange: ed with lines:' + IntToStr(ed.Strings.Count) +
+                   ' Needed: '+ IntToStr(Needed));
   //Add if there are less
   while states.Count<Needed do
     states.Add(nil);
@@ -126,7 +165,8 @@ begin
   if states.Count > Needed then
     states.Count:= Needed;
   //Calculate the first line modified, and lines shown
-  firstModified := 0;  //??? No better information
+  //firstModified := 0;
+  DebugLn('EditorChange: text modified from:' + IntToStr(firstModified+1));
   stateFrom := firstModified div LINES_PER_STATE;
   scanFrom :=  stateFrom * LINES_PER_STATE;
   scanTo := ed.LineBottom;
@@ -138,24 +178,9 @@ begin
     //following lines
     hlt.Range:=states[stateFrom];
   end;
-  DebugLn('  scanning from:' + IntToStr(scanFrom)+ ' to ' + IntToStr(scanTo));
-  //scan necessary lines
-  for i:= scanFrom to scanTo do
-  begin
-    hlt.SetLine(ed.Strings.lines[i], i);
-    while true do begin
-      if hlt.GetEol then begin
-        if i mod LINES_PER_STATE = 0 then begin
-          DebugLn('    saving state for line:' + IntToStr(i));
-//          lineas.Objects[i] := TObject(hlt.Range);
-          states[i mod LINES_PER_STATE] := hlt.Range;
-        end;
-        break;
-      end;
-      hlt.Next;
-    end;
-  end;
+  ScanLines(ed, scanFrom, scanTo);
   //showmessage('onchange');
+  firstModified := MaxInt;  //clean flag
 end;
 
 end.
